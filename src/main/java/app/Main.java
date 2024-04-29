152f4d9636919b969a681c7398feb71bce7463ca
SimoneSkovgaard <simonerskovgaard@gmail.com>
2024-04-29 10:57:53 +0200

    Javalin - thymeleaf - ConnectionPool

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,9 +1,37 @@
 package app;
 
+import app.config.SessionConfig;
+import app.config.ThymeleafConfig;
+import app.persistence.ConnectionPool;
+import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinThymeleaf;
+
 public class Main {
 
-    public static void main(String[] args) {
-        
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "postgres";
+    private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
+    private static final String DB = "databasename";
+
+    private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
+
+    public static void main(String[] args)
+    {
+        // Initializing Javalin and Jetty webserver
+
+        Javalin app = Javalin.create(config -> {
+            config.staticFiles.add("/public");
+            config.jetty.modifyServletContextHandler(handler ->  handler.setSessionHandler(SessionConfig.sessionConfig()));
+            config.fileRenderer(new JavalinThymeleaf(ThymeleafConfig.templateEngine()));
+        }).start(7070);
+
+        // Routing
+
+        app.get("/", ctx ->  ctx.render("index.html"));
     }
+        
+
 
 }
+
+
