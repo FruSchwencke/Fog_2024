e4b815a022bc2eda238797b63eee72fb3f8a22e9
SimoneSkovgaard <simonerskovgaard@gmail.com>
2024-05-01 13:05:33 +0200

    entities: user and order

---
+++ src/main/java/app/entities/Order.java
@@ -0,0 +1,12 @@
+package app.entities;
+
+public class Order {
+
+    private int orderId;
+    private int lenght;
+    private int width;
+    private String status;
+    private double totalprice;
+
+
+}
--- src/main/java/app/entities/User.java
+++ src/main/java/app/entities/User.java
@@ -1,4 +1,18 @@
 package app.entities;
 
 public class User {
+
+private int userID;
+private String firstName;
+private String lastName;
+private String email;
+private String password;
+private String address;
+private String phonenumber;
+private int zipcode;
+private int role;
+
+
+
 }
+
