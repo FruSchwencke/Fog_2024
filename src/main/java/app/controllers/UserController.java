fe7603a6f62dae89e2840426d6b47aef9d2c0163
SimoneSkovgaard <simonerskovgaard@gmail.com>
2024-05-02 10:24:08 +0200

    Login - UserMapper og UserController

--- src/main/java/app/controllers/UserController.java
+++ src/main/java/app/controllers/UserController.java
@@ -1,4 +1,40 @@
 package app.controllers;
 
+import app.entities.User;
+import app.exceptions.DatabaseException;
+import app.persistence.ConnectionPool;
+import app.persistence.UserMapper;
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+
 public class UserController {
+
+    public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
+        app.post("login", ctx -> login(ctx, connectionPool));
+        app.get("login", ctx -> ctx.render("login"));
+
+    }
+
+
+    private static void login(Context ctx, ConnectionPool connectionPool) {
+        String email = ctx.formParam("email");
+        String password = ctx.formParam("password");
+
+
+        try {
+            User user = UserMapper.login(email, password, connectionPool );
+            if (user.getRole() ==1){
+                ctx.sessionAttribute("currentUser", user);
+
+                ctx.render("salesperson_page.html");
+            }else {
+                ctx.sessionAttribute("currentUser", user);
+                ctx.render("customer_page.html");
+            }
+        } catch (DatabaseException e) {
+            ctx.attribute("message", e.getMessage());
+            ctx.render("login.html");
+        }
+
+    }
 }
--- src/main/java/app/entities/User.java
+++ src/main/java/app/entities/User.java
@@ -2,7 +2,7 @@ package app.entities;
 
 public class User {
 
-private int userID;
+private int userId;
 private String firstName;
 private String lastName;
 private String email;
@@ -12,7 +12,20 @@ private String phonenumber;
 private int zipcode;
 private int role;
 
+    public User(int userID, String firstName, String lastName, String email, String password, String address, String phonenumber, int zipcode, int role) {
+        this.userId = userID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.address = address;
+        this.phonenumber = phonenumber;
+        this.zipcode = zipcode;
+        this.role = role;
+    }
 
-
+    public int getRole() {
+        return role;
+    }
 }
 
