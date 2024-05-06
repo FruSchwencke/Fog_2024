518ec21fd590a18a0f7b41b3e2666b9af74a2f33
Jaqueline <jaqueline@schwencke.dk>
2024-05-06 14:19:38 +0200

    working on calculatePosts method, still in progress

--- src/main/java/app/services/Calculate.java
+++ src/main/java/app/services/Calculate.java
@@ -8,10 +8,9 @@ import app.persistence.MaterialMapper;
 public class Calculate {
 
 
-    public static void calcPost(int length, ConnectionPool connectionPool){
+    public static void calculatePosts(int length, int width, ConnectionPool connectionPool){
 
         //Get material
-
         int materialId = 1601;
         try {
             Material material = MaterialMapper.getMaterialById(1601, connectionPool);
@@ -19,9 +18,26 @@ public class Calculate {
             throw new RuntimeException(e);
         }
 
-        // Calculate posts
+        // Calculate posts on length
+        // offset is the space in the front and back of the carport
+        int offsetL1 = 1000;
+        int offsetL2 = 300;
+        //offsets are not subtracted from the length, it is not as vital for the design, like it is for width.
+        int maxlength = 6000;
+        //the ceil method rounds a number UP to the nearest Integer, and not in either direction like the round() method.
+        int quantityByLength = (int) Math.ceil(length - (offsetL1 +offsetL2) / maxlength);
+
+        // Calculate posts based on width
+        // offset is the space in the sides of the carport
+        int offsetW1 = 350;
+        int offsetW2 = 350;
+        //offsets are subtracted from maxWidth, so we accurately can calculate if the need for at post in the middle to hold the roof, really is required.
+        int maxWidth = 6000 - (offsetW1+offsetW2);
+        int quantityByWidth = (int) Math.ceil(width - (offsetW1 +offsetW2) / maxWidth);
 
 
+        //create result for customized order
+        int quantityOfPosts = quantityByWidth * quantityByLength;
 
 
 
