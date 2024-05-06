8ac947272c570553e8a4897200ea10e7e684d142
Jaqueline <jaqueline@schwencke.dk>
2024-05-06 11:24:47 +0200

    changed the button back, changed how Ordercontroller retrieves the user input and the currentUser - preparing to change login flow, by removing the option to customize a carport on index.html, the user now has to login in, to get access to customization

--- src/main/java/app/controllers/OrderController.java
+++ src/main/java/app/controllers/OrderController.java
@@ -1,10 +1,12 @@
 package app.controllers;
 
 import app.entities.Order;
+import app.entities.User;
 import app.exceptions.DatabaseException;
 import app.persistence.ConnectionPool;
 import app.persistence.MaterialMapper;
 import app.persistence.OrderMapper;
+import app.persistence.UserMapper;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 
@@ -33,16 +35,19 @@ import java.util.List;
         public static void customizeCarportRoute(Context ctx, ConnectionPool connectionPool) {
 
             //These parameters represent the choices that the user has to make and the ability to send forward a text with remark or wishes, when customizing a carport
-                String width = ctx.queryParam("choose_width");
-                String length = ctx.queryParam("choose_length");
-                String roof = ctx.queryParam("choose_roof");
-                String input = ctx.queryParam("text_input");
+                String width = ctx.formParam("choose_width");
+                String length = ctx.formParam("choose_length");
+                //String roof = ctx.formParam("choose_roof");
+                String input = ctx.formParam("text_input");
+
+            // retrieving the userId
+                User currentUser = ctx.sessionAttribute("currentUser");
 
 
             //
             try {
 
-                int orderId = OrderMapper.createOrder(10, Integer.parseInt(width),Integer.parseInt(length), input, connectionPool);
+                int orderId = OrderMapper.createOrder(currentUser.getUserId(), Integer.parseInt(width),Integer.parseInt(length), input, connectionPool);
                 MaterialMapper.createMaterialLine(1, orderId,1, connectionPool);
                 MaterialMapper.createMaterialLine(1, orderId,2, connectionPool);
                 MaterialMapper.createMaterialLine(1, orderId,3, connectionPool);
