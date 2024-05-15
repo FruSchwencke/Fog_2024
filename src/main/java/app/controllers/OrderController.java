d52d3635f2b918a1ef674c852a91249341a30ac9
SimoneSkovgaard <simonerskovgaard@gmail.com>
2024-05-15 09:29:44 +0200

    updateTotalPrice now showing

--- src/main/java/app/controllers/OrderController.java
+++ src/main/java/app/controllers/OrderController.java
@@ -135,6 +135,13 @@ public class OrderController {
                 double newTotalPrice = Double.parseDouble(ctx.formParam("newTotalPrice"));
                 if (newTotalPrice >= 0) {
                     OrderMapper.updateTotalPrice(orderId, newTotalPrice, connectionPool);
+                    Order orderDetails = OrderMapper.getOrderDetails(orderId, connectionPool);
+                    User userInformation = OrderMapper.getUserInformation(orderId, connectionPool);
+                    double totalPrice = OrderMapper.getTotalPrice(orderId, connectionPool);
+
+                    ctx.attribute("orderDetails", orderDetails);
+                    ctx.attribute("userInformation", userInformation);
+                    ctx.attribute("totalPrice", totalPrice);
                     ctx.attribute("message", newTotalPrice + " er nu prisen for ordre nr " + orderId + ".");
                     ctx.render("order_details.html");
                 }else {
